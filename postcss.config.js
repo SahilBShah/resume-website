22ba86de58fcd4a75902c0f88ff08b50effa68e9
Sahil Shah <sahil2699@gmail.com>
2019-12-07 01:40:26 -0600

    Adding files

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,21 @@
-module.exports = {
-    plugins: [
-        require('cssnano')({
-            preset: 'default',
-        }),
-    ],
+'use strict';
+module.exports = (ctx) => {
+  return ({
+    parser: ctx.file.extname === '.sss' ? 'sugarss' : false,
+    plugins: {
+      cssnano: {
+        discardComments: {
+          removeAll: true
+        },
+        filterPlugins: false,
+        discardDuplicates: true,
+        discardOverridden: true,
+        minifySelectors: true,
+        mergeRules: true,
+        core: true,
+        reduceIdents: false,
+        svgo: false
+      }
+    }
+  });
 };
